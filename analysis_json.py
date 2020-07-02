4753979d60e075a2c6bda1319af0899dbee2d52e
longmaoaiali <13213102491@163.com>
2020-07-02 23:57:51 +0800

    http request data, using json module to analysis request data

---
+++ analysis_json.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+def main():
+    resp = requests.get('http://api.tianapi.com/guonei/?key=APIKey&num=10')
+    data_model = json.loads(resp.text)
+    for news in data_model['newslist']:
+        print(news['title'])
+
+
+if __name__ == '__main__':
+    main()
+	
+'''
+- `dump` - 将Python对象按照JSON格式序列化到文件中
+- `dumps` - 将Python对象处理成JSON格式的字符串
+- `load` - 将文件中的JSON数据反序列化成对象
+- `loads` - 将字符串的内容反序列化成Python对象
+'''
