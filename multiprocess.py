a2a8db33dcfc2abf24d60f11ee7c197fc5369072
longmaoaiali <13213102491@163.com>
2020-07-08 23:56:01 +0800

    fix lowlevel mistake

--- multiprocess.py
+++ multiprocess.py
@@ -5,7 +5,7 @@ from time import time, sleep
 
 def download_task(filename):
 	print('start download , pid = %d.' % getpid())
-	time_to_download = randint(5,10) //随机生成整数：[a-b]区间的整数（包含两端）
+	time_to_download = randint(5,10) #随机生成整数：[a-b]区间的整数（包含两端）
 	sleep(time_to_download)
 	print('download file spend %d' % time_to_download)
 	
@@ -17,7 +17,7 @@ def main():
 	p2 = Process(target=download_task, args=('1.avi', ))
 	p2.start()
 	
-	p1.join() //等待子进程结束再继续向下执行
+	p1.join() #等待子进程结束再继续向下执行
 	p2.join()
 	
 	end = time()
